7e2bff02390cb613016b678f623a68605f2ec080
alisher1129 <ali.sher@vaival.com>
2024-08-30 12:16:11 +0500

    add create request and make payment fields

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -3,12 +3,13 @@
 import React, { useState } from "react";
 import { prepareContractCall } from "thirdweb";
 import { useSendTransaction, useActiveAccount } from "thirdweb/react";
-import { contractFunding } from "../../../utils/contract";
+import { contractFunding } from "../../utils/contract";
 
 function AdminPage() {
   const [addDescription, setDescription] = useState("");
   const [addRecipient, setRecipient] = useState("");
   const [addValue, setValue] = useState("");
+  const [addRequestNumber, setRequestNumber] = useState("");
 
   const account = useActiveAccount();
   const { mutate: sendTransaction } = useSendTransaction();
@@ -26,39 +27,83 @@ function AdminPage() {
     }
   };
 
+  const makePaymentFunction = async () => {
+    try {
+      const transaction = prepareContractCall({
+        contract: contractFunding,
+        method: "function makePayment()",
+        params: [addRequestNumber],
+      });
+      sendTransaction(transaction);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
   return (
     <>
-      <div className="flex flex-col justify-center  mt-16">
-        <form className="font-[sans-serif] max-w-4xl mx-auto">
-          <input
-            type="text"
-            placeholder="Description"
-            value={addDescription}
-            onChange={(e) => setDescription(e.target.value)}
-            className="mt-2 px-20 py-4 bg-[#f0f1f2] focus:bg-transparent text-black w-full text-sm border outline-[#007bff] rounded transition-all"
-          />
-          <input
-            type="text"
-            placeholder="Recipient"
-            value={addRecipient}
-            onChange={(e) => setRecipient(e.target.value)}
-            className="mt-2 px-20 py-4 bg-[#f0f1f2] focus:bg-transparent text-black w-full text-sm border outline-[#007bff] rounded transition-all"
-          />
-          <input
-            type="text"
-            placeholder="Value"
-            value={addValue}
-            onChange={(e) => setValue(e.target.value)}
-            className="mt-2 px-20 py-4 bg-[#f0f1f2] focus:bg-transparent text-black w-full text-sm border outline-[#007bff] rounded transition-all"
-          />
-          <button
-            type="button"
-            onClick={createNewRequest}
-            className="mt-4 px-6 py-3 text-sm w-full bg-[#007bff] hover:bg-[#006bff] text-white rounded transition-all"
-          >
-            Add
-          </button>
-        </form>
+      <div className="flex flex-col justify-center  mt-4">
+        {/* CREATE REQUEST  */}
+        <div>
+        <div className="flex justify-center mb-6 font-extrabold font-serif text-5xl">
+Create Request          </div>
+          <form className="font-[sans-serif] max-w-4xl mx-auto">
+            <input
+              type="text"
+              placeholder="Enter Description"
+              value={addDescription}
+              onChange={(e) => setDescription(e.target.value)}
+              className="mt-2 px-20 py-4 bg-[#f0f1f2] focus:bg-transparent text-black w-full text-sm border outline-[#007bff] rounded transition-all"
+            />
+            <input
+              type="text"
+              placeholder="Enter Recipient"
+              value={addRecipient}
+              onChange={(e) => setRecipient(e.target.value)}
+              className="mt-2 px-20 py-4 bg-[#f0f1f2] focus:bg-transparent text-black w-full text-sm border outline-[#007bff] rounded transition-all"
+            />
+            <input
+              type="text"
+              placeholder="Enter Value"
+              value={addValue}
+              onChange={(e) => setValue(e.target.value)}
+              className="mt-2 px-20 py-4 bg-[#f0f1f2] focus:bg-transparent text-black w-full text-sm border outline-[#007bff] rounded transition-all"
+            />
+            <button
+              type="button"
+              onClick={createNewRequest}
+              className="mt-4 px-6 py-3 text-sm w-full bg-[#007bff] hover:bg-[#006bff] text-white rounded transition-all"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+
+        {/* MAKE PAYMENT */}
+        <div className="mt-4">
+          <div className="flex justify-center mb-6 font-extrabold font-serif text-5xl">
+            Make Payment
+          </div>
+          <form className="font-[sans-serif] max-w-4xl mx-auto">
+            <div className="flex items-center">
+              <input
+                type="text"
+                value={addRequestNumber}
+                onChange={(e) => setRequestNumber(e.target.value)}
+                placeholder="Enter Request Number"
+                className="px-20 py-4 bg-[#f0f1f2] focus:bg-transparent text-black w-full text-sm border outline-[#007bff] rounded transition-all"
+              />
+            </div>
+
+            <button
+              type="button"
+              onClick={makePaymentFunction}
+              className="mt-4 px-6 py-3 text-sm w-full bg-[#007bff] hover:bg-[#006bff] text-white rounded transition-all"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ConnectButton } from "thirdweb/react";
+import { client } from "@/app/client";
+import { chain } from "@/app/chain";
+
+function NavBar() {
+  return (
+    <>
+      <header className="  flex shadow-md border-b-2 border-gray-400 py-4 px-4 sm:px-10 bg-white font-[sans-serif] min-h-[70px] tracking-wide relative z-50">
+        <div className="flex flex-wrap items-center justify-between gap-5 w-full">
+          <a href="javascript:void(0)">
+            <img
+              src="https://cdn.prod.website-files.com/623b471f31c24a4a20eb3ca7/6294e95995e10fe9dd8ede4c_DecubateLogo-SVG.svg"
+              alt="logo"
+              className="w-36"
+            />
+          </a>
+          <div className="flex max-lg:ml-auto space-x-3">
+            <ConnectButton client={client} chain={chain} />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default NavBar;
